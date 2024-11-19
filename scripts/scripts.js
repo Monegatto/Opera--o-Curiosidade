2333e13e523f6e2fbd5204fe96160bfd168949f8
Monegatto <jp.monegatto@protonmail.com>
2024-11-19 10:54:01 -0300

    Nome de usuário logado e refatoração da função de pesquisa. O nome do usuário logado agora é dinamicamente escrito com o uso de sessionStorage e a função de pesquisar é aplicada aos campos de busca com o carregamento da pagina

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,13 @@
 /**
+ * Quando a tela for carregada o nome do usuário logado vai ser buscado no sessionStorage e a função de pesquisa será aplicada a todas as barras de pesquisa
+ */
+window.addEventListener('load', () => {
+    document.querySelector("p.userLogado").innerHTML = sessionStorage.getItem("Logado")
+
+    document.querySelector("input#ipesquisa").addEventListener('change', pesquisar)
+})
+
+/**
  * Função de pesquisa na tabela de cadastros
  */
 function pesquisar() {
@@ -27,54 +36,10 @@ loopCel:
                     encontrou = true
                     break
                 }
-            }
-            
+            } 
         }
         if(!encontrou)
             tabela.rows[i].style.display = "none"
-    }
-}
-
-/**
- * Busca a lista de usuários e a cria se ela não existir, depois a retorna
- */
-function carregarUsers(){
-    let users = JSON.parse(localStorage.getItem("Usuarios"))    //Busca a lista de usuários do sistema
-    if(users == null)                                           //Se não existir lista de usuários, cria ela
-        localStorage.setItem("Usuarios", "[]")
-    return users
-}
-
-/**
- * Busca a lista de cadastros e a cria se ela não existir, depois preenche a tabela com os dados dos colaboradores
- */
-function carregarCadastros(){
-    let cadastros = JSON.parse(localStorage.getItem("Cadastros"))   //Busca a lista de cadastros no sistema
-    if(cadastros == null)                                           //Se não existir lista de cadastros, cria ela
-        localStorage.setItem("Cadastros", "[]")
-
-    let tabela = window.document.querySelector("tbody")             //Encontra a tabela na página
-    for(let i = 0; i < cadastros.length; i++){
-        let user = cadastros[i]
-
-        let nome = document.createElement("td")
-        nome.append(user.nome)
-
-        let email = document.createElement("td")
-        email.append(user.email)
-
-        let ativo = document.createElement("td")
-        
-        if(!user.ativo){
-            ativo.className += " " + "inativo"
-            ativo.append("Inativo")
-        }else
-            ativo.append("Ativo")
-
-        let linha = document.createElement("tr")
-        linha.append(nome, email, ativo)
-
-        tabela.append(linha)
     }
 }
 
@@ -121,6 +86,119 @@ let validaLogin = (users, femail, fsenha) => {
 }
 
 /**
+ * Busca a lista de usuários e a cria se ela não existir, depois a retorna
+ */
+function carregarUsers(){
+    let users = JSON.parse(localStorage.getItem("Usuarios"))    //Busca a lista de usuários do sistema
+    if(users == null)                                           //Se não existir lista de usuários, cria ela
+        localStorage.setItem("Usuarios", "[]")
+    return users
+}
+
+/**
+ * Função de criar conta com validação para email
+ */
+function criarConta(){
+    let formData = new FormData(document.forms.formCadastro)
+    let users = carregarUsers()
+
+    let femail = formData.get("email")
+    if(femail == ""){                                                                  //Verifica se o campo de email está vazio
+        window.alert("Você deve fornecer um email para criar a conta")
+        return false
+    } else if(validaRepeticaoEmail(users, femail)){                                    //Verifica se já existe uma conta com esse email no sistema
+        window.alert("Uma conta com esse email já está cadastrada no sistema")
+        return false
+    } else if(!validaEmail(femail)){                                                   //Verifica se o email inserido é valido
+        window.alert("Email inválido, tente cadastrar outro")
+        return false
+    }
+
+    let fsenha = formData.get("senha")
+    if(fsenha == ""){                                                                  //Verifica se o campo de senha está vazio
+        window.alert("Você deve fornecer uma senha para criar a conta")
+        return false
+    }
+
+    let fnome = formData.get("nome")
+
+    let novaConta = {email: femail, senha: fsenha, nome: fnome}
+    users.push(novaConta)
+    localStorage.setItem("Usuarios", JSON.stringify(users))
+
+    setTimeout(() => window.location.replace("./login.html"))
+}
+
+/**
+ * Função de login com validações para email e senha
+ */
+function login(){
+    let formData = new FormData(document.forms.formLogin)
+    let users = carregarUsers()
+
+    let femail = formData.get("email")                              
+    if(femail == ""){                                               //Verifica se o campo de email está vazio
+        window.alert("Você deve fornecer um email para o login!")
+        return false
+    } else if(!validaRepeticaoEmail(users, femail)){                //Verifica se o email informado existe no sistema
+        window.alert("Email não encontrado no sistema!")
+        return false
+    }
+
+    let fsenha = formData.get("senha")
+    if(fsenha == ""){                                               //Verifica se o campo de senha está vazio
+        window.alert("Você deve fornecer uma senha para o login!")
+        return false
+    }else if(!validaLogin(users, femail, fsenha)){                  //Valida se o email e senha informado pertencem ao mesmo usuário
+        window.alert("Senha incorreta, tente novamente")
+        return false
+    }
+
+
+    users.forEach(user => {                                         //Percorre a lista de usuários do sistema e, quando encontra o usuário logado, o adiciona ao sesssioStorage
+        if(user.email == femail)
+            sessionStorage.setItem("Logado", user.nome)
+    });
+
+    setTimeout(() => window.location.replace("./home.html"))
+}
+
+/**
+ * Busca a lista de cadastros e a cria se ela não existir, depois preenche a tabela com os dados dos colaboradores
+ */
+function carregarCadastros(){
+    let cadastros = JSON.parse(localStorage.getItem("Cadastros"))   //Busca a lista de cadastros no sistema
+    if(cadastros == null)                                           //Se não existir lista de cadastros, cria ela
+        localStorage.setItem("Cadastros", "[]")
+
+    let tabela = window.document.querySelector("tbody")             //Encontra a tabela na página
+    cadastros.forEach(cadastro => {
+        let user = cadastro
+
+        let nome = document.createElement("td")
+        nome.append(user.nome)
+
+        let email = document.createElement("td")
+        email.append(user.email)
+
+        let ativo = document.createElement("td")
+        
+        if(!user.ativo){
+            ativo.className += " " + "inativo"
+            ativo.append("Inativo")
+        }else
+            ativo.append("Ativo")
+
+        let linha = document.createElement("tr")
+        linha.append(nome, email, ativo)
+
+        linha.addEventListener('click', modCad)
+
+        tabela.append(linha)
+    });
+}
+
+/**
  * Função que cadastra novos colaboradores no sistema
  */
 function cadastrar(){
@@ -163,64 +241,4 @@ function cadastrar(){
     localStorage.setItem("Cadastros", JSON.stringify(cadastros))
 
     setTimeout(() => window.location.replace("./home.html"))
-}
-
-/**
- * Função de login com validações para email e senha
- */
-function login(){
-    let formData = new FormData(document.forms.formLogin)
-    let users = carregarUsers()
-
-    let femail = formData.get("email")                              
-    if(femail == ""){                                               //Verifica se o campo de email está vazio
-        window.alert("Você deve fornecer um email para o login!")
-        return false
-    } else if(!validaRepeticaoEmail(users, femail)){                //Verifica se o email informado existe no sistema
-        window.alert("Email não encontrado no sistema!")
-        return false
-    }
-
-    let fsenha = formData.get("senha")
-    if(fsenha == ""){                                               //Verifica se o campo de senha está vazio
-        window.alert("Você deve fornecer uma senha para o login!")
-        return false
-    }else if(!validaLogin(users, femail, fsenha)){                  //Valida se o email e senha informado pertencem ao mesmo usuário
-        window.alert("Senha incorreta, tente novamente")
-        return false
-    }
-
-    setTimeout(() => window.location.replace("./home.html"))
-}
-
-/**
- * Função de criar conta com validação para email
- */
-function criarConta(){
-    let formData = new FormData(document.forms.formCadastro)
-    let users = carregarUsers()
-
-    let femail = formData.get("email")
-    if(femail == ""){                                                                  //Verifica se o campo de email está vazio
-        window.alert("Você deve fornecer um email para criar a conta")
-        return false
-    } else if(validaRepeticaoEmail(users, femail)){                                    //Verifica se já existe uma conta com esse email no sistema
-        window.alert("Uma conta com esse email já está cadastrada no sistema")
-        return false
-    } else if(!validaEmail(femail)){                                                   //Verifica se o email inserido é valido
-        window.alert("Email inválido, tente cadastrar outro")
-        return false
-    }
-
-    let fsenha = formData.get("senha")
-    if(fsenha == ""){                                                                  //Verifica se o campo de senha está vazio
-        window.alert("Você deve fornecer uma senha para criar a conta")
-        return false
-    }
-
-    let novaConta = {email: femail, senha: fsenha}
-    users.push(novaConta)
-    localStorage.setItem("Usuarios", JSON.stringify(users))
-
-    setTimeout(() => window.location.replace("./login.html"))
 }
