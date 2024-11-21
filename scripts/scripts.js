5a518f4cfc353c60622c59da900260d227422844
Monegatto <jp.monegatto@protonmail.com>
2024-11-21 11:18:07 -0300

    Adicionadas edição e exclusão de colaboradores e ajustes nos formularios -Novos scripts de edição e exclusão de colaboradores, junto da nova tela de edição de cadastros que pode ser acessada ao clicar em qualquer linha das tabelas -Cadastro de colaboradores reorganizado para permitir o preenchimento do formulario de edição -Validação da idade dos colaboradores caso o referido campo não esteja vazio

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,7 +19,7 @@ function pesquisar() {
 
         tabela.rows[i].style.display = ""
         encontrou = false
-loopCel:
+loopCell:
         for(let j = 0; j < tabela.rows[i].cells.length; j++){
             let conteudoCelula = tabela.rows[i].cells[j].textContent.toUpperCase()
 
@@ -28,7 +28,7 @@ loopCel:
                 for(let k = 0; k < nomeCompleto.length; k++){
                     if(nomeCompleto[k].startsWith(termo)){
                         encontrou = true
-                        break loopCel
+                        break loopCell
                     }
                 }
             } else {
@@ -44,7 +44,7 @@ loopCel:
 }
 
 /**
- * Percorre todo a lista de nomes cadastrados e compara o nome de cada entrada com o nome fornecido
+ * Percorre todo a lista de nomes cadastrados e compara o nome de cada entrada com o nome recebido
  */
 let validaRepeticaoNome = (cadastros, fnome) => {
     for(let i = 0; i < cadastros.length; i++){
@@ -55,7 +55,7 @@ let validaRepeticaoNome = (cadastros, fnome) => {
 }
 
 /**
- * Percorre a lista de usuários do sistema e compara o email de cada um com o email fornecido
+ * Percorre a lista de usuários do sistema e compara o email de cada um com o email recebido
  */
 let validaRepeticaoEmail = (users, femail) => {
     for(let i = 0; i < users.length; i++){
@@ -66,7 +66,7 @@ let validaRepeticaoEmail = (users, femail) => {
 }
 
 /**
- * Valida o email fornecido e retorna falso para inválido e verdadeiro para válido
+ * Valida o email recebido e retorna falso para inválido e verdadeiro para válido
  */
 let validaEmail = (femail) =>{
     if(!((/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/).test(femail)))                //Verifica se o email inserido é valido
@@ -75,7 +75,7 @@ let validaEmail = (femail) =>{
 }
 
 /**
- * Percorre toda a lista de usuários e verifica se a senha e email fornecidos pertencem ao mesmo usuário
+ * Percorre toda a lista de usuários e verifica se a senha e email recebidos pertencem ao mesmo usuário
  */
 let validaLogin = (users, femail, fsenha) => {
     for(let i = 0; i < users.length; i++){
@@ -155,10 +155,10 @@ function login(){
     }
 
 
-    users.forEach(user => {                                         //Percorre a lista de usuários do sistema e, quando encontra o usuário logado, o adiciona ao sesssioStorage
+    users.forEach(user => {                                         //Percorre a lista de usuários do sistema e, quando encontra o usuário logado, o adiciona ao sessionStorage
         if(user.email == femail)
             sessionStorage.setItem("Logado", user.nome)
-    });
+    })
 
     setTimeout(() => window.location.replace("./home.html"))
 }
@@ -192,10 +192,10 @@ function carregarCadastros(){
         let linha = document.createElement("tr")
         linha.append(nome, email, ativo)
 
-        linha.addEventListener('click', modCad)
+        linha.addEventListener('click', setCad)
 
         tabela.append(linha)
-    });
+    })
 }
 
 /**
@@ -227,17 +227,130 @@ function cadastrar(){
     }
 
     let fidade = formData.get("idade")
+    if(fidade != ""){                                                       //Se o campo de idade não estiver vazio uma validação é realizada
+        if(isNaN(fidade)){                                                  //Verifica se o valor inserido é numérico
+            window.alert("A idade deve ser um número!")
+            return false
+        } else if(fidade < 18 || fidade > 100){                             //Verifica se a idade inserida está dentro dos limites esperados
+            window.alert("Somente pessoas entre 18 e 100 anos podem ser cadastradas no sistema")
+            return false
+        }
+    }
+
     let fendereco = formData.get("endereco")
     let finfo = formData.get("info")
     let finter = formData.get("inter")
     let fsenti = formData.get("senti")
     let fvalor = formData.get("valor")
+    let iativo = window.document.querySelector("input#iativo").checked      //Tratamento especial ao checkbox
 
+    let novoCadastro = {nome: fnome, idade: fidade, email: femail, endereco: fendereco, info: finfo, ativo: iativo, inter: finter, senti: fsenti, valor: fvalor}
+    cadastros.push(novoCadastro)
+    localStorage.setItem("Cadastros", JSON.stringify(cadastros))
+
+    setTimeout(() => window.location.replace("./home.html"))
+}
+
+/**
+ *  Função que armazena na sessão o nome e email (valores únicos) do usuário que será editado ou excluído
+ */ 
+function setCad(){
+    sessionStorage.setItem("CadNome", this.children[0].innerHTML)
+    sessionStorage.setItem("CadEmail", this.children[1].innerHTML)
+    window.location.href = "./editarCadastro.html"
+}
+
+/**
+ * Função que atribui os valores do colaborador aos campos do formulário para edição
+ */
+function getCad(){
+    let cadastros = JSON.parse(localStorage.getItem("Cadastros"))           //Busca a lista de cadastros no sistema
+    let editavel                                                            //Cadastrado que vai ser editado
+    cadastros.forEach(cadastro => {
+        if(cadastro.nome == sessionStorage.getItem("CadNome"))
+            editavel = cadastro
+    });
+
+    const { elements } = document.querySelector('form')                     //Recebe todos os elementos do formulário
+
+    for(const [key, value] of Object.entries(editavel)){                    //Retorna as chaves e valores do usuario que será editado
+        const field = elements.namedItem(key)                       //Recebe os campos do formulário que tem o mesmo nome dos valores dos atributos do colaborador a ser editado
+        field.value = value                                                 //Atribui os valores do colaborador aos campos do formulario
+    }
+
+    window.document.querySelector("input#iativo").checked = editavel.ativo  //O input checkbox possui comportamento diferente e portanto deve ser tratado diferentemente
+}
+
+/**
+ * Função que edita os cadastros no sistema
+ */
+function editar(){
+    let formData = new FormData(document.forms.formCadastro)
+    let cadastros = JSON.parse(localStorage.getItem("Cadastros"))
+    let CadNome = sessionStorage.getItem("CadNome")
+    let CadEmail = sessionStorage.getItem("CadEmail")
+
+    let fnome = formData.get("nome")
+    if(fnome == ""){                                                                        //Lida com campo de nome vazio
+        window.alert("Você precisa fornecer um nome para a edição!")
+        return false
+    } else if(validaRepeticaoNome(cadastros, fnome) && fnome != CadNome){                   //Verifica se o nome é repetido e diferente do colaborador sendo editado
+        window.alert("Esse nome já está registrado no sistema!")
+        return false
+    }                                                                     
+    let femail = formData.get("email")
+    if(femail == ""){                                                                       //Lida com o campo de email vazio
+        window.alert("Você deve fornecer um email para o cadastro")
+        return false
+    }else if(validaRepeticaoEmail(cadastros, femail) && femail != CadEmail){                //Verifica se o email é repetido e diferente do colaborador sendo editado
+        window.alert("Uma conta com esse email já existe")
+        return false
+    }
+    if(!validaEmail){                                                                       //Verifica se o email inserido é valido
+        window.alert("Email inválido, tente cadastrar outro")
+        return false
+    }
+
+    let fidade = formData.get("idade")
+    if(fidade != ""){                                                                       //Se o campo de idade não estiver vazio uma 
+        if(isNaN(fidade)){                                                                  //Verifica se o valor inserido é numéricovalidação é realizada
+            window.alert("A idade deve ser um número!")
+            return false
+        } else if(fidade < 18 || fidade > 100){                                             //Verifica se a idade inserida está dentro dos limites esperados
+            window.alert("Somente pessoas entre 18 e 100 anos podem ser cadastradas no sistema")
+            return false
+        }
+    }
+
+    let fendereco = formData.get("endereco")
+    let finfo = formData.get("info")
+    let finter = formData.get("inter")
+    let fsenti = formData.get("senti")
+    let fvalor = formData.get("valor")
     let iativo = window.document.querySelector("input#iativo").checked
 
-    let fcebola = {inter: finter, senti: fsenti, valor: fvalor}
-    let novoCadastro = {nome: fnome, idade: fidade, email: femail, endereco: fendereco, info: finfo, ativo: iativo, cebola: fcebola}
-    cadastros.push(novoCadastro)
+    let cadAtualizado = {nome: fnome, idade: fidade, email: femail, endereco: fendereco, info: finfo, ativo: iativo, inter: finter, senti: fsenti, valor: fvalor}
+    
+    for(let i = 0; i < cadastros.length; i++){
+        if(cadastros[i].nome == sessionStorage.getItem("CadNome"))
+            cadastros[i] = cadAtualizado
+    };
+
+    localStorage.setItem("Cadastros", JSON.stringify(cadastros))
+
+    setTimeout(() => window.location.replace("./home.html"))
+}
+
+/**
+ * Função que exclui um dos cadastros no sistema
+ */
+function excluir(){
+    let cadastros = JSON.parse(localStorage.getItem("Cadastros"))
+    for(let i = 0; i < cadastros.length; i++){
+        if(cadastros[i].nome == sessionStorage.getItem("CadNome"))
+            cadastros.splice(i, 1)
+    }
+    
     localStorage.setItem("Cadastros", JSON.stringify(cadastros))
 
     setTimeout(() => window.location.replace("./home.html"))
